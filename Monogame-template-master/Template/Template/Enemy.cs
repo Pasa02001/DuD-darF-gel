259088ccd4cb720c373b9021b308dfe6c4fb6614
Pasa02001 <54437734+Pasa02001@users.noreply.github.com>
2020-10-13 10:50:43 +0200

    DuDödarFågel

---
+++ Monogame-template-master/Template/Template/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Template
+{
+    class Enemy
+    {
+       
+            Texture2D Bomb;
+            float angle = (float)Math.Atan(1);
+            Vector2 EnemyPos = new Vector2(200, 200);
+            public Enemy(Texture2D Enemy)
+            {
+                this.Bomb = Enemy;
+            }
+            private void Update(GameTime gameTime)
+            {
+
+            }
+            public void Draw(SpriteBatch spriteBatch)
+            {
+
+                spriteBatch.Draw(Bomb, new Rectangle((int)EnemyPos.X, (int)EnemyPos.Y, 70, 70), null, Color.White, angle, new Vector2() , SpriteEffects.None, 0);
+
+            }
+        
+    }
+}
